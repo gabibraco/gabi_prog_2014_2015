eb7460d24544d20cd154f2a605b873ff9e966805
gabibraco <gabi.bc.01@gmail.com>
2014-11-07 15:50:57 +0100

    Constructures
    Signed-off-by: gabibraco <gabi.bc.01@gmail.com>

---
+++ ConstructoresObjetos/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+	//definicion de los objetos
+	public static SegundaClase s1;
+	public static SegundaClase s2;
+	
+	public static void main(String[] args) {
+		//creacion de objetos
+		s1=new SegundaClase(3);
+		s2=new SegundaClase(6);
+	}
+
+}
---
+++ ConstructoresObjetos/src/SegundaClase.java
@@ -0,0 +1,16 @@
+
+public class SegundaClase {
+	//Variable
+	int num=0;
+
+	public SegundaClase(int valor) {
+		System.out.println(" El valor de un num antes de guardar es"+num);
+					
+		num=valor;
+		
+		System.out.println(" El valor de un num antes de guardar es"+num);
+				
+		
+	}
+
+}
