9ae4d0a446d0baae68e553046a0b3977e2efd21e
gabibraco <gabi.bc.01@gmail.com>
2015-03-25 21:05:06 +0100

    Proyecto5(V1)
    Signed-off-by: gabibraco <gabi.bc.01@gmail.com>

--- GameDB/src/GamesDBApp.java
+++ GameDB/src/GamesDBApp.java
@@ -2,12 +2,11 @@ import Vista.*;
 
 
 public class GamesDBApp {
-	public static VistaPrincipal VistaPrincipal;
+	
 	public static void main(String[] args) {
 		VistaApp frame = new VistaApp();
 		frame.setVisible(true);
-		VistaPrincipal=new VistaPrincipal();
-		frame.setVisible(true);
+		
 
 	}
 
